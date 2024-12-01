a3079c94bd702debe6dbfcad92def1b464bfd0b7
Hakan Sahin <hakonsahin@gmail.com>
2024-12-02 02:59:50 +0300

    day2 completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,26 @@
+import { Route, Switch } from "react-router-dom/cjs/react-router-dom.min";
 import "./App.css";
 import Card from "./components/Card";
 import Header from "./components/Header";
 import Order from "./components/Order";
-
+import MainPage from "./components/MainPage";
+import Success from "./components/Success";
 function App() {
   return (
     <>
-      <Header />
-      <Order />
-      <Card />
+      <Switch>
+        <Route exact path="/">
+          <MainPage />
+        </Route>
+        <Route exact path="/order">
+          <Header />
+          <Order />
+          <Card />
+        </Route>
+        <Route exact path="/success">
+          <Success />
+        </Route>
+      </Switch>
     </>
   );
 }
