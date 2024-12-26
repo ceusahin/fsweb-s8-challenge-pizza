ac11df666e55f53598c0db1abda9f6515132b845
Hakan Sahin <hakonsahin@gmail.com>
2024-12-26 20:34:23 +0300

    images updated for vercel

--- src/components/AdvertCard1.jsx
+++ src/components/AdvertCard1.jsx
@@ -1,23 +1,32 @@
 import "../styles/advertcard.css";
+import kart1 from "../../Assets/Iteration-2-aseets/cta/kart-1.png";
+import kart2 from "../../Assets/Iteration-2-aseets/cta/kart-2.png";
+import kart3 from "../../Assets/Iteration-2-aseets/cta/kart-3.png";
 
 export default function AdvertCard1() {
   return (
     <section className="advert1">
       <article className="cards">
-        <div className="card-left">
+        <div className="card-left" style={{ backgroundImage: `url(${kart1})` }}>
           <h2>Özel</h2>
           <h2>Lezzetus</h2>
           <p>Position: Absolute Acı Burger</p>
           <button className="advert-button">SİPARİŞ VER</button>
         </div>
         <div className="card-right">
-          <div className="card-right-1">
+          <div
+            className="card-right-1"
+            style={{ backgroundImage: `url(${kart2})` }}
+          >
             <h2>
               Hackathlon <br /> Burger Menü
             </h2>
             <button className="advert-button">SİPARİŞ VER</button>
           </div>
-          <div className="card-right-2">
+          <div
+            className="card-right-2"
+            style={{ backgroundImage: `url(${kart3})` }}
+          >
             <h2>
               <span>Çoooook</span> hızlı <br /> npm gibi kurye
             </h2>
